ee09eee32142f530db99ac63fd74108af602ffc5
Dipen Patel <107390824+DOodle25@users.noreply.github.com>
2025-01-20 11:02:37 +0530

    Update index.js

--- login-verify-mail/Student-Backend/index.js
+++ login-verify-mail/Student-Backend/index.js
@@ -18,8 +18,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
     origin: '*',
-    methods: '*',
-    allowedHeaders: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
 
