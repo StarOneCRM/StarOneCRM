817dc40d80579da2596d2c654b68d597770e77d5
Dipen Patel <107390824+DOodle25@users.noreply.github.com>
2025-01-20 17:41:44 +0530

    port

--- login-verify-mail/Student-Backend/index.js
+++ login-verify-mail/Student-Backend/index.js
@@ -8,7 +8,7 @@ const userRoutes = require("./routes/user.routes");
 const profileRoutes = require("./routes/profile.routes")
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 // Database connection
 connection();
